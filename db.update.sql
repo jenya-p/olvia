885da6f73e8395ffb1ff3b34e11da73999e666e0
Evgeny Poloznov <jenyapol@mail.ru>
2017-06-26 22:58:41 +0300

    fix

--- db.update.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `content_content2course` (
-	`content_id` INT NOT NULL,
-	`course_id` INT NOT NULL,
-	`priority` INT NULL,
-	PRIMARY KEY (`content_id`, `course_id`)
-)
-COLLATE='utf8_general_ci'
-;
